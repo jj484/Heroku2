de802d798bbc830d042bd3a7fd9ffca858540dd5
Preetpal <w3irdg@gmail.com>
2022-03-17 21:30:47 -0400

    Fixes + Overhaul

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_pages = Blueprint('simple_pages', __name__,
-                        template_folder='templates',static_folder='static')
+                        template_folder='mytemplates',static_folder='static')
 
 
 @simple_pages.route('/', defaults={'page': 'index'})
 @simple_pages.route('/<page>')
 def show(page):
     try:
-        return render_template('%s.html' % page)
+        return render_template('%s.html' % page, active_page = page)
     except TemplateNotFound:
         abort(404)
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,49 +4,37 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Git</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'<a id="git" class="nav-link" href="/git">GitHub/Git</a>' in response.data
+    assert b'<a id="docker" class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a id="flask" class="nav-link" href="/flask">Python/Flask</a>' in response.data
+    assert b'<a id="cicd" class="nav-link" href="/cicd">CI/CD</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Preetpal Singh" in response.data
+    assert b"My Site" in response.data
 
-def test_request_about(client):
-    """This makes the index page"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"About Page" in response.data
-
-def test_request_page1(client):
-    """This makes the index page"""
-    response = client.get("/page1")
+def test_request_git(client):
+    """This makes the git page"""
+    response = client.get("/git")
     assert response.status_code == 200
     assert b"Git" in response.data
 
-def test_request_page2(client):
-    """This makes the index page"""
-    response = client.get("/page2")
+def test_request_docker(client):
+    """This makes the docker page"""
+    response = client.get("/docker")
     assert response.status_code == 200
     assert b"Docker" in response.data
 
 def test_request_page3(client):
-    """This makes the index page"""
-    response = client.get("/page3")
+    """This makes the flask page"""
+    response = client.get("/flask")
     assert response.status_code == 200
     assert b"Flask" in response.data
 
 def test_request_page4(client):
-    """This makes the index page"""
-    response = client.get("/page4")
+    """This makes the Continuous Integration/Continuous Deployment page"""
+    response = client.get("/cicd")
     assert response.status_code == 200
-    assert b"Continuous Integration / Continuous Deployment" in response.data
-
-def test_request_page_not_found(client):
-    """This makes the index page"""
-    response = client.get("/page5")
-    assert response.status_code == 404
+    assert b"Continuous Integration/Continuous Deployment" in response.data
